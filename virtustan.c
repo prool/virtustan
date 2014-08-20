cdbf15496d385904002cb3f676970a1c26076b89
prool <proolix@gmail.com>
2014-08-20 17:37:57 +0300

    изучаю git. я тупой

--- virtustan.c
+++ virtustan.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
-printf("Virtustan 0.1\n\nVirtustan rulez!\n\n");
+printf("Virtustan 0.1\n\n");
+printf("Virtustan rulez!\n\n");
+printf("В чащах юга жил бы цитрус!\n\n");
 return 0;
 }
