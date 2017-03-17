f8e3d9956d2726d1a095daf8b33a3a31eb8e5136
prool <proolix@gmail.com>
2017-03-17 22:52:21 +0200

    BUGFIX in tintin++, module prool_log

--- tintin++/src/prooltran.c
+++ tintin++/src/prooltran.c
@@ -94,7 +94,7 @@ void prool_log(char *message)
 FILE *fp;
 
 fp=fopen("prooltin.log","a");
-if (fp==NULL) printf("prooltin: can't open log\n");
+if (fp==NULL) {printf("prooltin: can't open log\n"); total_log=0; return;}
 fprintf(fp,"%i %s %s\r\n",getpid(),ptime(),message);
 fclose(fp);
 }
